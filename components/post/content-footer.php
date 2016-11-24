0cd0ce03c777ae596fc207101f18b54a9c9de4bc
Phuc Nguyen Minh <minhphuc429@gmail.com>
2016-11-24 14:43:15 +0700

    first commit core _s starter theme

---
+++ components/post/content-footer.php
@@ -0,0 +1,3 @@
+	<footer class="entry-footer">
+		<?php rewrite_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
---
+++ components/post/content-meta.php
@@ -0,0 +1,3 @@
+		<div class="entry-meta">
+			<?php rewrite_posted_on(); ?>
+		</div><!-- .entry-meta -->
